058e1bbd3b61aea14b0ba4f92ea679491f9b2dd6
ashique-alam <ashiquemaner786@gmail.com>
2021-05-19 14:45:04 +0530

    Thirsty added

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello{
 	public static void main(String []args){
 		System.out.println("hello world");
 		System.out.println("This is modified file");
+		for(int i=0;i<10;i++){
+			System.out.println("Thirsty");
+		}
 	}
 }
